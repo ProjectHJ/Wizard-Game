01e69672c30280c2770186cfd98f2e290cb416e9
ProjectHJ <illnation.3@gmail.com>
2017-08-16 00:30:23 -0500

    Formatted Correctly & Added Game Loop

--- src/com/comlu/projecthj/Game.java
+++ src/com/comlu/projecthj/Game.java
@@ -6,37 +6,27 @@ import com.comlu.projecthj.window.Window;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
-	
-	private static int 
-			MAJOR_VERSION = 1, 
-			BUILD_VERSION = 0, 
-			MINOR_VERSION = 0;
-	
-	private static String 
-			BUILD = "Alpha";
-	private static String 
-			NAME = "Wizard Game";
-	
-	private static String 
-			TITLE = NAME + " " + 
-					BUILD + "_" + 
-					MAJOR_VERSION + "." + 
-					BUILD_VERSION + "." + 
-					MINOR_VERSION;
-	
+
+	private static int MAJOR_VERSION = 1, BUILD_VERSION = 0, MINOR_VERSION = 0;
+
+	private static String BUILD = "Alpha";
+	private static String NAME = "Wizard Game";
+
+	private static String TITLE = NAME + " " + BUILD + "_" + MAJOR_VERSION + "." + BUILD_VERSION + "." + MINOR_VERSION;
+
 	private boolean isRunning = false;
 	private Thread thread;
-	
-	public Game() { 
-		new Window(800, 600, TITLE, this); 
+
+	public Game() {
+		new Window(800, 600, TITLE, this);
 	}
-	
+
 	public void start() {
 		isRunning = true;
 		thread = new Thread(this);
 		thread.start();
 	}
-	
+
 	public void stop() {
 		isRunning = false;
 		try {
@@ -45,11 +35,42 @@ public class Game extends Canvas implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
-	public void run() {	
-		
+
+	public void run() {
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 100000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while (isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				delta--;
+			}
+			render();
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frames = 0;
+			}
+		}
+		stop();
 	}
-	
+
+	public void render() {
+
+	}
+
+	public void tick() {
+
+	}
+
 	public static void main(String[] args) {
 		new Game();
 	}
--- src/com/comlu/projecthj/window/Window.java
+++ src/com/comlu/projecthj/window/Window.java
@@ -7,13 +7,13 @@ import javax.swing.JFrame;
 import com.comlu.projecthj.Game;
 
 public class Window {
-	public Window(int width, int height, String title, Game game) { 
+	public Window(int width, int height, String title, Game game) {
 		JFrame frame = new JFrame(title);
-		
+
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
-		
+
 		frame.add(game);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
