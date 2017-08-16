075ce585f30716e5cff3d9ecfa52437b452cf575
ProjectHJ <illnation.3@gmail.com>
2017-08-16 00:45:30 -0500

    Added Graphics & Random Fixes and/or Tweaks

--- src/com/comlu/projecthj/Game.java
+++ src/com/comlu/projecthj/Game.java
@@ -1,6 +1,8 @@
 package com.comlu.projecthj;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 
 import com.comlu.projecthj.window.Window;
@@ -8,7 +10,7 @@ import com.comlu.projecthj.window.Window;
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
-	private static int MAJOR_VERSION = 1, BUILD_VERSION = 0, MINOR_VERSION = 0;
+	private static int MAJOR_VERSION = 1, BUILD_VERSION = 1, MINOR_VERSION = 0;
 
 	private static String BUILD = "Alpha";
 	private static String NAME = "Wizard Game";
@@ -18,8 +20,11 @@ public class Game extends Canvas implements Runnable {
 	private boolean isRunning = false;
 	private Thread thread;
 
+	private static int width = 960, height = width / 16 * 9;
+	
 	public Game() {
-		new Window(800, 600, TITLE, this);
+		new Window(width, height, TITLE, this);
+		start();
 	}
 
 	public void start() {
@@ -55,8 +60,8 @@ public class Game extends Canvas implements Runnable {
 			}
 			render();
 			frames++;
-			
 			if (System.currentTimeMillis() - timer > 1000) {
+				System.out.println("isRunning" + isRunning+ " : " + frames);
 				timer += 1000;
 				frames = 0;
 			}
@@ -72,6 +77,14 @@ public class Game extends Canvas implements Runnable {
 			this.createBufferStrategy(numBuffers);
 			return;
 		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, width, height);
+		
+		g.dispose();
+		bs.show();
 	}
 
 	public void tick() {
