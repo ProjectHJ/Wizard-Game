5d260e4b13a5d9aedb15d2dedc3fcdb8ccbd0f36
ProjectHJ <illnation.3@gmail.com>
2017-08-16 00:08:46 -0500

    Fix
    I forgot to add the 'new Game();' to the main method. Idiot

--- src/com/comlu/projecthj/Game.java
+++ src/com/comlu/projecthj/Game.java
@@ -5,8 +5,17 @@ import java.awt.Canvas;
 import com.comlu.projecthj.window.Window;
 
 public class Game extends Canvas implements Runnable {
-	private static final long serialVersionUID = 6674382819211038206L;
-	public Game() { new Window(1000, 563, "Wizard Game", this); }
-	public void run() {	}
-	public static void main(String[] args) { }
+	private static final long serialVersionUID = 1L;
+	
+	public Game() { 
+		new Window(800, 600, "Wizard Game", this); 
+	}
+	
+	public void run() {	
+		
+	}
+	
+	public static void main(String[] args) {
+		new Game();
+	}
 }
--- src/com/comlu/projecthj/window/Window.java
+++ src/com/comlu/projecthj/window/Window.java
@@ -9,6 +9,7 @@ import com.comlu.projecthj.Game;
 public class Window {
 	public Window(int width, int height, String title, Game game) { 
 		JFrame frame = new JFrame(title);
+		
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
