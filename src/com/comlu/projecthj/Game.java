6419a0fc667333522b8badac324e106b18cd8dc3
ProjectHJ <illnation.3@gmail.com>
2017-08-16 00:03:44 -0500

    Second Commit

--- src/com/comlu/projecthj/Game.java
+++ src/com/comlu/projecthj/Game.java
@@ -1,6 +1,12 @@
 package com.comlu.projecthj;
 
-public class Game {
-	public Game() { }
+import java.awt.Canvas;
+
+import com.comlu.projecthj.window.Window;
+
+public class Game extends Canvas implements Runnable {
+	private static final long serialVersionUID = 6674382819211038206L;
+	public Game() { new Window(1000, 563, "Wizard Game", this); }
+	public void run() {	}
 	public static void main(String[] args) { }
 }
